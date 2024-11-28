207b939b431a1fb493f100ea3ee4a309d91bcbb6
Kartheepan91 <rkartheepan0@gmail.com>
2024-11-28 07:11:54 +0530

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Azure_web_app_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
